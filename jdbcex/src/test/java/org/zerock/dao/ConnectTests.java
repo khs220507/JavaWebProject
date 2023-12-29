f46b5c493144aca8b9efac44ac1170f5a738e6ec
khs220507 <khs220507@gmail.com>
2023-12-29 22:46:36 +0900

    complete db

---
+++ jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
@@ -0,0 +1,21 @@
+package org.zerock.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectTests {
+
+    @Test
+    public void testConnection() throws Exception{
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/webdb",
+                "webuser",
+                "220507");
+        Assertions.assertNotNull(connection);
+        connection.close();
+    }
+}
